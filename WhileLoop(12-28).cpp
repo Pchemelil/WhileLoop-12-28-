1d1201b5d0cc52cee29a88f5df85356220de18c4
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-11-16 21:43:11 +0300

    Add files via upload

---
+++ WhileLoop(12-28).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int number = 12;
+	
+	
+	while(number<=28){
+	
+	cout<< number<<",";	
+	number = number + 2;
+	    
+	}
+	
+
+	
+	return 0;
+}
